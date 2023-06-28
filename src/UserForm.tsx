a2a46e1a7fc633105ec4fc7c0367c096b62f766a
mhevyk <maxxevyk7@gmail.com>
2023-06-28 19:18:21 +0300

    Initial commit

---
+++ src/UserForm.tsx
@@ -0,0 +1,8 @@
+import { ChangeEvent } from "react";
+
+export default function UserForm() {
+  function handleSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+  return <form onSubmit={handleSubmit}>User Form</form>;
+}
