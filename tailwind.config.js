35f2665757d22539124791cc10593c668c45a3f2
mhevyk <maxxevyk7@gmail.com>
2023-06-29 23:43:19 +0300

    Added tailwind animations plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+import TailwindAnimated from "tailwindcss-animated";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [TailwindAnimated],
 };
